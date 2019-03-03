90ce17079d8bd7a6eed17d426e5c2460f32fda2c
r888800009 <r888800009@users.noreply.github.com>
2019-03-03 16:28:04 +0800

    Update test.sh

--- DS2-1/test.sh
+++ DS2-1/test.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 g++ main.cpp
-echo "
-1 101
-1 102
-2 101
-2 102
-"|./a.out
+files=( {101..105} 1000 )
+
+rm maxheap.txt deap.txt
+
+for i in "${files[@]}"
+do
+    echo "=== $i ===" >> maxheap.txt
+    echo 1 $i |./a.out| grep "\[" >> maxheap.txt
+done
+
+for i in "${files[@]}"
+do
+    echo "=== $i ===" >> deap.txt
+    echo 2 $i |./a.out| grep "\[" >> deap.txt
+done
 
 echo
 g++ main.cpp -DDEBUG1111
-echo "0" |./a.out
+echo "0" |./a.out >> debug.txt
