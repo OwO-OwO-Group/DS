10d1e375f803d0c2524d49687dcd80ff995e2e55
r888800009 <r888800009@users.noreply.github.com>
2019-03-03 17:38:32 +0800

    Update test-time

--- DS2-1/test-time.sh
+++ DS2-1/test-time.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-g++ main.cpp
-time ./a.out <<< "1 105 2 105"
+rm time.txt
+
+function testTime()
+{
+    g++ main.cpp
+    for i in {1..40}
+    do
+        (( time ./a.out <<< "1 105 2 105" ) | grep s) 2>> time.txt
+    done
+}
+
+git checkout master main.cpp
+testTime
+mv time.txt time.txt.2
+
+git checkout d7955a5 main.cpp
+testTime
+git checkout master main.cpp
