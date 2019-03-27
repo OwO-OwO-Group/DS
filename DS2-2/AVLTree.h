99ea7289e8a982b84b16a1a4e405959dd16114c1
r888800009 <r888800009@users.noreply.github.com>
2019-03-28 02:55:39 +0800

    Add AVLTree.h and Tree23.h

---
+++ DS2-2/AVLTree.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _AVLTREE_H_
+#define _AVLTREE_H_
+#include <Data.h>
+
+class AVLTree {
+public:
+    void insert(Data);
+};
+
+#endif
+
---
+++ DS2-2/Tree23.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef _TREE23_H_
+#define _TREE23_H_
+#include <Data.h>
+
+class Tree23 {
+
+public:
+    void insert(Data);
+};
+
+#endif
+
