3ced97974b84fe32088896bda2a14da62d0aaea1
r888800009 <r888800009@users.noreply.github.com>
2019-04-02 02:36:56 +0800

    Add two test case

--- DS2-2/Test.cpp
+++ DS2-2/Test.cpp
@@ -10,7 +10,18 @@ void doTest(bool result, string meg)
     cout << "[" << (result ? "done" : "fail") << "]:" << meg << endl;
 }
 
-bool testAVLInsert() { return false; }
+bool testAVLInsert()
+{
+    // double rotation case 1
+    // RL case 4, 2, 6, 1, 3, 5, 7, 16, 15
+    // right inorder 1, 2, 3, 4, 5, 6, 7, 15, 16
+
+    // double rotation case 2
+    // RL case 4, 2, 6, 1, 3, 5, 7, 16, 15, 14
+    // right inorder 1, 2, 3, 4, 5, 6, 7, 14, 15, 16
+
+    return false;
+}
 bool test23Insert() { return false; }
 
 void debug()
