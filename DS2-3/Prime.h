68c31edd73b8d392d804e9cd10e1eba8760b6354
r888800009 <r888800009@users.noreply.github.com>
2019-04-29 09:20:51 +0800

    Add Prime.h

---
+++ DS2-3/Prime.h
@@ -0,0 +1,7 @@
+// 第10組 106127116 許逸翔 10612150 林詠翔 資訊二甲
+// must to use -std=c++11 or higher version
+#include <vector>
+static vector<int> prime;
+
+int findPrimeMoreThan(int n);
+bool isPrime(int n);
